5615f32872062771dd25efd57ea241446facfba9
evertonSouzaMeli <everton.vsilva@mercadolivre.com>
2022-03-01 17:21:49 -0300

    feat: work with Interval

--- src/test/java/FluxTest.java
+++ src/test/java/FluxTest.java
@@ -7,6 +7,7 @@ import reactor.core.publisher.BaseSubscriber;
 import reactor.core.publisher.Flux;
 import reactor.test.StepVerifier;
 
+import java.time.Duration;
 import java.util.List;
 
 @Slf4j
@@ -133,5 +134,39 @@ public class FluxTest {
         StepVerifier.create(fluxInteger)
                 .expectNext(1,2,3,4,5,6,7,8,9,10)
                 .verifyComplete();
+    }
+
+    @Test
+    public void fluxSubscriberIntervalOne() throws InterruptedException {
+        //Algo importante de salientar é que uma caracteristica do Reactor é ter uma
+        // Thread Secundaria onde haverá tudo o que pode bloquear a Thread Principal
+        var interval = Flux.interval(Duration.ofMillis(300))
+                                      //Pega um numero deternimado de elementos do Fluxo
+                                       .take(10)
+                                       .log();
+
+        interval.subscribe(i -> log.info("Number {}", i));
+
+        //Isso "CONGELA" a Thread principal para que a Secundaria a Ultrapasse e consiga executar a ação
+        Thread.sleep(30000);
+    }
+
+    @Test
+    public void fluxSubscriberIntervalTwo() throws InterruptedException {
+        //Virtual time tem uma restrição, o flux interval tem que ser criado dentro do metodo
+        StepVerifier.withVirtualTime(this::createInterval)
+                    .thenAwait(Duration.ofDays(2))
+                    //Verificar que nada está sendo executado antes do esperado
+                    .expectNoEvent(Duration.ofHours(24))
+                    .expectNext(0L)
+                    .expectNext(1L)
+                    .thenCancel()
+                    .verify();
+    }
+
+    private Flux<Long> createInterval() {
+        //todos os dias o ofDays() imprimira um resultado
+        return Flux.interval(Duration.ofDays(1))
+                .log();
     }
 }
