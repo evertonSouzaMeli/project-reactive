27d6469ab61a1cc1c4be0c505bc88b63bde61bae
evertonSouzaMeli <everton.vsilva@mercadolivre.com>
2022-03-01 19:54:32 -0300

    feat: start work with switchIfEmpty & defer

--- src/test/java/MonoTest.java
+++ src/test/java/MonoTest.java
@@ -1,9 +1,12 @@
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
 import java.util.Locale;
+import java.util.concurrent.atomic.AtomicLong;
 
 
 /**
@@ -150,5 +153,41 @@ public class MonoTest {
                 .log();
 
         StepVerifier.create(error).expectNext(name).verifyComplete();
+    }
+
+    @Test
+    public void switchIfEmptyOperator(){
+        var flux = emptyFlux().switchIfEmpty(Flux.just("not empty anymore")).log();
+
+        StepVerifier.create(flux)
+                    .expectSubscription()
+                    .expectNext("not empty anymore")
+                    .expectComplete()
+                    .verify();
+    }
+
+    @Test
+    public void deferOperator() throws Exception{
+        var just = Mono.just(System.currentTimeMillis());
+        //Defer é bom, toda vez que um subscribe entrar no Mono retorna valor atualizado
+        var defer = Mono.defer(() -> Mono.just(System.currentTimeMillis()));
+
+        //Apesar do uso de Delay, o resultado do tempo será o mesmo
+        // isso ocorre no momento da instancia, ele guarda o valor na memória
+        defer.subscribe(x -> log.info("time {}", x));
+        Thread.sleep(100);
+        defer.subscribe(x -> log.info("time {}", x));
+        Thread.sleep(100);
+        defer.subscribe(x -> log.info("time {}", x));
+        Thread.sleep(100);
+        defer.subscribe(x -> log.info("time {}", x));
+
+        AtomicLong atomicLong = new AtomicLong();
+        defer.subscribe(atomicLong::set);
+        Assertions.assertTrue(atomicLong.get() > 0L);
+    }
+
+    private Flux<Object> emptyFlux(){
+        return Flux.empty();
     }
 }
