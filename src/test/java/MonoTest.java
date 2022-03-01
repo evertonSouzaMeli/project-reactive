a12ca1f46f00ccdfdb19ebf7544ba83bbe5953c7
evertonSouzaMeli <everton.vsilva@mercadolivre.com>
2022-02-28 22:25:19 -0300

    feat: add Mono, StepVerifier

---
+++ src/test/java/MonoTest.java
@@ -0,0 +1,44 @@
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
+
+/**
+ * Reactive Streams
+ * 1. Asynchronous
+ * 2. Non-Blocking
+ * 3. Backpressure
+ * Publisher / Observable -> Quem emite os eventos
+ * Publisher é "cold"
+ * Publisher <- (subscribe) Subscriber
+ * Subscription é criado no momento que Subscribe se subinscreve no Publisher
+ * Subscription is created
+ * Publisher (onSubscribe on subscription) -> Subscriber
+ * Subscriber que faz o "Backpressure"
+ * Publisher chama o onNext no Subscriber
+ * until:
+ * 1. Publisher sends all the objects requested
+ * 2. Publisher envia tudo o que é possivel  (onComplete)
+ * 3. There is an error (onError) -> subscriber and subscription will be canceled
+ * * */
+@Slf4j
+public class MonoTest {
+
+    @Test
+    public void monoSubscriber(){
+        String name = "Everton Souza";
+        var monoString = Mono.just(name);
+
+        //mono.subscribe, serve para obter mais detalhes do Mono
+        monoString.log().subscribe();
+        log.info("\n -------STEP VERIFIER --------");
+        /**
+         * StepVerifier verifica os processos, parece o assertEquals
+         * termina a verificação com a função "verifyComplete()"
+         */
+        StepVerifier.create(monoString.log()).expectNext("Everton Souza").verifyComplete();
+        log.info("Mono {}", monoString);
+    }
+
+}
