fa546aa21d73f6fda56d49aad95c6b2551ee3837
evertonSouzaMeli <everton.vsilva@mercadolivre.com>
2022-03-01 19:29:51 -0300

    feat: add Callable for External API consume

--- src/test/java/OperatorsTest.java
+++ src/test/java/OperatorsTest.java
@@ -1,8 +1,13 @@
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 import reactor.core.scheduler.Schedulers;
 import reactor.test.StepVerifier;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 @Slf4j
 public class OperatorsTest {
@@ -133,5 +138,26 @@ public class OperatorsTest {
                 .expectSubscription()
                 .expectNext(1,2,3,4)
                 .verifyComplete();
+    }
+
+    //Esse methodo é muito bom para fazer chamadas para APIS externas
+    @Test
+    public void subscribeOnIO() throws Exception {
+        //Executa uma Thread em background quando a thread alvo estiver bloqueada
+        var list = Mono.fromCallable( () -> Files.readAllLines(Path.of("text-file")))
+                                                                    .log()
+                                                                    .subscribeOn(Schedulers.boundedElastic());
+
+        list.subscribe(string -> log.info("{}", string));
+
+        Thread.sleep(2000);
+
+        StepVerifier.create(list)
+                .expectSubscription()
+                .thenConsumeWhile(x -> {
+                    Assertions.assertFalse(x.isEmpty());
+                    log.info("Size {}", x.size());
+                    return true;
+                }).verifyComplete();
     }
 }
