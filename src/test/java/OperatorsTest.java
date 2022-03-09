d6c4ffdc0f5c2fc0333f5fca4ddc869f16811926
evertonSouzaMeli <everton.vsilva@mercadolivre.com>
2022-03-09 08:10:46 -0300

    feat: refactoring aula-13

--- src/test/java/OperatorsTest.java
+++ src/test/java/OperatorsTest.java
@@ -9,48 +9,39 @@ public class OperatorsTest {
 
     @Test
     public void subscribeOnSimple(){
-        var flux = Flux.range(1,4)
-                .map(i -> {
-                    log.info("Map 1 - Number {} on Thread {}", i, Thread.currentThread().getName());
-                    return i;
-                })
-                //Schedule single por ser uma Thread simples,
-                // subscribeOn afeta todu o fluxo, independente do antes e depois, serve para alterar a Thread de operação
-                // mudanto da main (thread principal) para outra declarada
-                .subscribeOn(Schedulers.single())
-                .map( i -> {
-                    log.info("Map 2 - Number {} on Thread {}", i, Thread.currentThread().getName());
-                    return i;
+        var flux = Flux.range(1,5)
+                .map(integer -> {
+                    log.info("Map 1 - Number {} on Thread {}", integer, Thread.currentThread().getName());
+                    return integer;
+                }).subscribeOn(Schedulers.boundedElastic())
+                .map(integer -> {
+                    log.info("Map 2 - Number {} on Thread {}", integer, Thread.currentThread().getName());
+                    return integer;
                 });
-
         StepVerifier.create(flux)
                 .expectSubscription()
-                .expectNext(1,2,3,4)
-                .verifyComplete();
+                .expectNext(1,2,3,4,5)
+                .expectComplete()
+                .verify();
     }
 
     @Test
     public void publishOnSimple() {
-        var flux = Flux.range(1, 4)
-                .map(i -> {
-                    log.info("Map 1 - Number {} on Thread {}", i, Thread.currentThread().getName());
-                    return i;
-                })
-                //publishOn afeta somente o que vai vir depois da função
-                .publishOn(Schedulers.boundedElastic())
-                .map(i -> {
-                    log.info("Map 2 - Number {} on Thread {}", i, Thread.currentThread().getName());
-                    return i;
+        var flux = Flux.range(1,5)
+                .map(integer -> {
+                    log.info("Map 1 - Number {} on Thread {}", integer, Thread.currentThread().getName());
+                    return integer;
+                }).publishOn(Schedulers.boundedElastic())
+                .map(integer -> {
+                    log.info("Map 2 - Number {} on Thread {} ", integer, Thread.currentThread().getName());
+                    return integer;
                 });
 
-        flux.subscribe();
-        flux.subscribe();
-
-        /*StepVerifier.create(flux)
+        StepVerifier.create(flux)
                 .expectSubscription()
-                .expectNext(1,2,3,4)
-                .verifyComplete();
-    }*/
+                .expectNext(1,2,3,4,5)
+                .expectComplete()
+                .verify();
     }
 
 }
